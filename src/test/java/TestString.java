85236afcef952152a526fab78e09aa1710121eae
MosheOgalbo <https://github.com/MosheOgalbo>
2022-03-24 03:30:42 +0200

    test 3

--- src/test/java/TestString.java
+++ src/test/java/TestString.java
@@ -1,4 +1,9 @@
 public class TestString {
     public  static  String PhoneNumberText = "כתובת אימייל לא חוקית";
     public static String EmailAddressText= "מספר טלפון לא חוקי";
+    public static String TextMatchSucceeded ="Messages that match the text";
+    public static String NoTextMatchSucceeded =" Messages do not match the text";
+    public static String NoMessage = "There are no element messages";
+
+
 }
